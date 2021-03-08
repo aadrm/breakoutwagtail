08326430bd752398b007b80314634a44858bf165
aadrm <antonioadriandev@gmail.com>
2021-03-08 15:15:51 +0000

    added database settings to production

--- breakout/settings/dev.py
+++ breakout/settings/dev.py
@@ -4,8 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%3!p!m0g*2w3cw3nt85h@y_o*ay%-!-@bh3f_j)j+c-wf$n1lm'
-
+SECRET_KEY = config['SECRET_KEY'] 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
 
--- breakout/settings/production.py
+++ breakout/settings/production.py
@@ -2,6 +2,20 @@ from .base import *
 
 DEBUG = False
 
+SECRET_KEY = config['SECRET_KEY'] 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wagtaildjango',
+        'USER': 'wagtaildjango',
+        'PASSWORD': config['DATABASE_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
 try:
     from .local import *
 except ImportError:
