78b51bd60974a2580f2aeeaa8772cba52bb6cb56
Antonio Adrian <antonioadriandev@gmail.com>
2022-10-16 17:02:47 +0000

    Skip payment amount check

--- apps/booking/handlers.py
+++ apps/booking/handlers.py
@@ -26,50 +26,46 @@ def show_me_the_money(sender, **kwargs):
         cart = Cart.objects.get(pk=ipn_obj.item_number)
         price = cart.total
 
-        print(ipn_obj.mc_gross, price)
-        if ipn_obj.mc_gross == price and ipn_obj.mc_currency == 'EUR':
-            payment = PaymentMethod.objects.get(method='paypal')
-            invoice = cart.invoice
-            if invoice:
-                invoice.full_name=ipn_obj.first_name + ' ' + ipn_obj.last_name
-                invoice.phone=ipn_obj.contact_phone
-                invoice.street=ipn_obj.address_street
-                invoice.post=ipn_obj.address_zip
-                invoice.city=ipn_obj.address_city
-                invoice.is_terms=True
-                invoice.is_privacy=True
-            else:
-                invoice = Invoice(
-                    full_name=ipn_obj.first_name + ' ' + ipn_obj.last_name,
-                    phone=ipn_obj.contact_phone,
-                    email=ipn_obj.payer_email,
-                    street=ipn_obj.address_street,
-                    post=ipn_obj.address_zip,
-                    city=ipn_obj.address_city,
-                    is_terms=True,
-                    is_privacy=True,
-                    payment=payment,
-                )
-
-            print(invoice.__dict__)
-            print(invoice.full_name, type(invoice.full_name))
-            print(invoice.phone, type(invoice.phone))
-            print(invoice.street, type(invoice.street))
-            print(invoice.post, type(invoice.post))
-            print(invoice.city, type(invoice.city))
-            print(invoice.is_terms, type(invoice.is_terms))
-            print(invoice.is_privacy, type(invoice.is_privacy))
-            invoice.save()
-            cart.invoice = invoice
-            cart.save()
-            payment = Payment(invoice=cart.invoice, amount=ipn_obj.mc_gross)
-            payment.save()
-
-            print('processing purchase')
-            cart.process_purchase()
-            send_cart_emails(cart)
+        payment = PaymentMethod.objects.get(method='paypal')
+        invoice = cart.invoice
+        if invoice:
+            invoice.full_name=ipn_obj.first_name + ' ' + ipn_obj.last_name
+            invoice.phone=ipn_obj.contact_phone
+            invoice.street=ipn_obj.address_street
+            invoice.post=ipn_obj.address_zip
+            invoice.city=ipn_obj.address_city
+            invoice.is_terms=True
+            invoice.is_privacy=True
         else:
-            print('invalid payment')
+            invoice = Invoice(
+                full_name=ipn_obj.first_name + ' ' + ipn_obj.last_name,
+                phone=ipn_obj.contact_phone,
+                email=ipn_obj.payer_email,
+                street=ipn_obj.address_street,
+                post=ipn_obj.address_zip,
+                city=ipn_obj.address_city,
+                is_terms=True,
+                is_privacy=True,
+                payment=payment,
+            )
+
+        print(invoice.__dict__)
+        print(invoice.full_name, type(invoice.full_name))
+        print(invoice.phone, type(invoice.phone))
+        print(invoice.street, type(invoice.street))
+        print(invoice.post, type(invoice.post))
+        print(invoice.city, type(invoice.city))
+        print(invoice.is_terms, type(invoice.is_terms))
+        print(invoice.is_privacy, type(invoice.is_privacy))
+        invoice.save()
+        cart.invoice = invoice
+        cart.save()
+        payment = Payment(invoice=cart.invoice, amount=ipn_obj.mc_gross)
+        payment.save()
+
+        print('processing purchase')
+        cart.process_purchase()
+        send_cart_emails(cart)
 
     else:
         pass
